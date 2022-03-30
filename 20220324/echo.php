beeb5be6d797989814f58fb26439aed1054a0d6f
MafeR222 <101894149+MafeR222@users.noreply.github.com>
2022-03-30 16:50:09 -0500

    ejercicios
    ejercicios de formularios

---
+++ 20220324/echo.php
@@ -0,0 +1,22 @@
+<html>
+  <head>
+  </head>
+   <body>
+       <h1>Hola soy un titulo</h1>
+       <?php
+           
+           // INCIA CODIGO PHP 
+           // Esto es un comentario
+             /* Todo lo que esta aqui deentro 
+             se considera un comentario 
+             */
+             // COMANDO  ECHO DEVUELVE UN VALOR
+
+             echo "Hola soy un parrafo";
+             echo "<br>";      // salto de linea 
+             echo 3+3;     /* realiza operaciones en php
+                        que no se puede en html. */
+             // TERMINA PHP
+             ?>
+    </body>
+</html>  
---
+++ 20220324/variables.php
@@ -0,0 +1,12 @@
+<?php 
+   //definimos una variable con texto 
+    $miVariable = "Hola variable";
+    // visualizamos la variable en el explorador
+    echo $miVariable;
+    // otro ejemplo variable miValor igual a 2 
+    echo "<br>";
+    $miValor = 2;
+    //visualizamos sin embargo realizamos una multiplicaion * 3
+    echo 3*$miValor
+
+?>
